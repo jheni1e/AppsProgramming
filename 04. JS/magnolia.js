be673134908d4c618ef0d38588f2d22369af8bc4
Jhenie <highhalma@gmail.com>
2025-08-20 08:55:16 -0300

    basicos do js

---
+++ 04. JS/magnolia.js
@@ -0,0 +1,12 @@
+// variáveis
+let soufoda = "letisca"; //var local
+var nacama = "te esculacho"; //var global/nao usado com frequencia
+const pi = 3.14159265358979; //constante
+
+// objetos
+const letica = {
+    nome: "letição",
+    idade: 18,
+    namorados: [ 'gw', 'alemao']
+};
+console.log(letica.nome + " ama muito seu homem bad boy " + letica.namorados[0]);
