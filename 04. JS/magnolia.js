8bb8d03806ec3ae7820d18058a4e879c2d5f59e7
Jhenie <highhalma@gmail.com>
2025-08-20 08:59:48 -0300

    array, functions

--- 04. JS/magnolia.js
+++ 04. JS/magnolia.js
@@ -3,6 +3,9 @@ let soufoda = "letisca"; //var local
 var nacama = "te esculacho"; //var global/nao usado com frequencia
 const pi = 3.14159265358979; //constante
 
+// array
+let namorados = ['gw','alemao','jhenizao','malu','1.30','malandrao','bob, o construtor']
+
 // objetos
 const letica = {
     nome: "letição",
@@ -10,3 +13,16 @@ const letica = {
     namorados: [ 'gw', 'alemao']
 };
 console.log(letica.nome + " ama muito seu homem bad boy " + letica.namorados[0]);
+
+// função comum
+function soma(a, b) {
+    return a + b;
+}
+
+// função anônima
+const mult = function (a, b) {
+    return a * b;
+};
+
+// arrow function
+const sub = (a, b) => a - b;
