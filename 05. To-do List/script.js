b33ff2492e99d6a45e99e1a899fc6dbfc8ed12fa
Jhenie <highhalma@gmail.com>
2025-08-21 09:42:24 -0300

    começando a implementar a deleção de task

--- 05. To-do List/script.js
+++ 05. To-do List/script.js
@@ -1,15 +1,27 @@
 const addTask = () => {
     let nametask = document.getElementById('addtask').value;
 
-    if (nametask != null) {
+    if (nametask != "") {
         let newtask = document.createElement('h4');
+        
         newtask.className = 'task';
         newtask.textContent = nametask;
+        
+        let divtask = document.createElement('div');
+        divtask.className = 'divtask';
+
+        let img = document.createElement('img');
+        img.src = "garbage.png";
+        img.id = "imggarbage";
+        img.style = "height: 60px;"
+
+        divtask.appendChild(newtask);
+        divtask.appendChild(img);
 
         setTimeout(() => {
             document.getElementById('addtask').value = "";    
         }, 50);
 
-        document.getElementById('tasklist').appendChild(newtask);
+        document.getElementById('tasklist').appendChild(divtask);
     }
 };
