ad901879d19f9537ead098e2789183fb2319809b
Jhenie <highhalma@gmail.com>
2025-09-03 11:38:08 -0300

    react-router-dom routes and links

--- 08. React Basics v2/vite-project/src/App.jsx
+++ 08. React Basics v2/vite-project/src/App.jsx
@@ -1,17 +1,43 @@
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
+import { Link, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Study from './pages/study.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
 
 function App() {
-  const [num, setNum] = useState(0)
+  // const [num, setNum] = useState(0)
 
-  useEffect(() => {
-    console.log(`O conteúdo da página foi alterado.\nEstado da variável: ${num}`)
-  }, [num])
+  // useEffect(() => {
+  //   console.log(`O conteúdo da página foi alterado.\nEstado da variável: ${num}`)
+  // }, [num])
+
+  // return (
+  //   <>
+  //     <h1>{num}</h1>
+  //     <button onClick={() => setNum(num + 1)}>Clique aqui</button>
+
+  //     <br />
+  //     <Link to="/pages/study">Clique aqui lele</Link>
+
+  //     <Routes>
+  //       <Route path="/pages/study" element={<Study />} />
+  //     </Routes>
+    // </>
 
   return (
     <>
-      <h1>{num}</h1>
-      <button onClick={() => setNum(num + 1)}>Clique aqui</button>
+      <nav>
+        <Link to="/">Início</Link> |{" "}
+        <Link to="/sobre">Sobre</Link> |{" "}
+        <Link to="/study">Estudo</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<About />} />
+        <Route path="/study" element={<Study />} />
+      </Routes>
     </>
   )
 }
---
+++ 08. React Basics v2/vite-project/src/pages/study.jsx
@@ -0,0 +1,3 @@
+export default function Study() {
+  return <h2>lelele se eu te pegar vc vai ve</h2>
+}
