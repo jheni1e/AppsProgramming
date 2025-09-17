15f7c26da1b46d2e416c5dbb1858c2aac63a3459
Jhenie <highhalma@gmail.com>
2025-09-17 08:47:19 -0300

    feat: routes, browserrouter, nav

--- 11. React SPA/react-class/src/App.jsx
+++ 11. React SPA/react-class/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <header>
+        <nav>
+          <Link to="/">Ínicio</Link>
+          <Link to="/about">Sobre</Link>
+        </nav>
+      </header>
+
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
     </>
