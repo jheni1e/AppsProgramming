78286fe2109111fb0dadd443fa9963ac39b6af5e
Jhenie <highhalma@gmail.com>
2025-09-25 11:40:18 -0300

    final fixes

--- 14. React Requests/reactrequests/src/pages/User.jsx
+++ 14. React Requests/reactrequests/src/pages/User.jsx
@@ -7,14 +7,13 @@ export default function User() {
 
     if (!user) return <p>Nenhum usuário selecionado.</p>
 
-
     return (
         <>
         <div className="m-10 bg-slate-200 text-black w-100 h-50 rounded-lg flex justify-center flex-col">
             <h2 className="font-bold text-3xl">Perfil do Usuário:</h2>
-            <p className="text-md"><strong>Nome:</strong> {user.name}</p>
-            <p className="text-md"><strong>Email:</strong> {user.email}</p>
-            <p className="text-md"><strong>Telefone:</strong> {user.phone}</p>
+            <p className="text-lg"><strong>Nome:</strong> {user.name}</p>
+            <p className="text-lg"><strong>Email:</strong> {user.email}</p>
+            <p className="text-lg"><strong>Telefone:</strong> {user.phone}</p>
         </div>
         </>
     );
