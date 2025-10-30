a86e8765fe347ffadbd4ce07686654b92dd4abc8
Jhenie <highhalma@gmail.com>
2025-10-30 09:19:21 -0300

    playing with shapes, fonts and input

--- 17. React Native Basics/react-native-basics/app/index.tsx
+++ 17. React Native Basics/react-native-basics/app/index.tsx
@@ -1,14 +1,26 @@
 import { HelloWave } from '@/components/hello-wave';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import { useState } from 'react';
 
 export default function HomeScreen() {
+  const [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_700Bold,
+  });
+
+  const [text, setText] = useState("");
+
   return (
     <View>
       <Text style={styles.title}>
-        oiiiiiii
+        oiiiiiii malu te amo
         <HelloWave />
       </Text>
       <View style={styles.square} />
+      <Text style={styles.text}>fala paizao {text}</Text>
+      <TextInput placeholder="Digite aqui" style={styles.input}
+      onChange={(e) => setText(e.target.value)} />
     </View>
   );
 }
@@ -18,6 +30,7 @@ const styles = StyleSheet.create({
     color: 'black',
     fontSize: 40,
     fontWeight: 'bold',
+    fontFamily: 'Montserrat_400Regular',
     textAlign: 'center',
     margin: 12
   },
@@ -27,6 +40,20 @@ const styles = StyleSheet.create({
     backgroundColor: 'thistle',
     alignSelf: 'center',
     margin: 10,
-    borderRadius: 8
+    borderRadius: 150
+  },
+  input: {
+    alignSelf: 'center',
+    margin: 20,
+    width: 150,
+    height: 30,
+    textAlign: 'center',
+    borderWidth: 1,
+    borderColor: 'gray',
+    outline: 'none'
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 20
   }
 })
